0ebb094ba07e842db7035fe6e195460c69d2565b
Kenny Lu <kenny@whynotkenny.com>
2018-09-05 08:59:32 -0400

    rename menu / new eslint rules / add menu items

---
+++ src/common/components/NavBar/constants.js
@@ -0,0 +1,28 @@
+const menuItems = [
+  {
+    displayName: 'Home',
+    path: '/',
+  },
+  {
+    displayName: 'Monthly Specials',
+    path: '/special-offers',
+  },
+  {
+    displayName: 'Signature Pizzas',
+    path: '/signature-pizzas',
+  },
+  {
+    displayName: 'Sides',
+    path: '/sides',
+  },
+  {
+    displayName: 'Catering',
+    path: '/catering',
+  },
+  {
+    displayName: 'Contact',
+    path: '/contact',
+  },
+];
+
+export default menuItems;
