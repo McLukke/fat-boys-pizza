d5b7d33118c750ed1760af4e41024ca7f9ded943
Kenny Lu <kenny@whynotkenny.com>
2018-09-06 15:42:51 -0400

    setup other pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Page from 'components/Page';
 import NavBar from 'components/NavBar';
-import { Switch, Route } from 'react-router-dom';
+import { withRouter, Switch, Route } from 'react-router-dom';
 
 import HomePage from 'home';
 import SignaturePizzasPage from 'signaturepizzas';
@@ -9,32 +9,35 @@ import CateringPage from 'catering';
 import ContactPage from 'contact';
 import ErrorPage from '404';
 
-const App = (props) => {
-  const { isLoading } = props;
+class App extends React.Component {
+  render() {
+    const { isLoading } = this.props;
 
-  if (isLoading) {
-    return <div>Loading</div>;
+    if (isLoading) {
+      return <div>Loading</div>;
+    }
+
+    return (
+      <Page.Wrapper>
+        <NavBar />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/signature-pizzas" component={SignaturePizzasPage} />
+          <Route path="/sides" component={SidesPage} />
+          <Route path="/catering" component={CateringPage} />
+          <Route path="/contact" component={ContactPage} />
+
+          <Route component={ErrorPage} />
+        </Switch>
+      </Page.Wrapper>
+    );
   }
-
-  return (
-    <Page.Wrapper>
-      <NavBar />
-
-      <Switch>
-        <Route path="/" component={HomePage} />
-        <Route path="/signature-pizzas" component={SignaturePizzasPage} />
-        <Route path="/sides" component={SidesPage} />
-        <Route path="/catering" component={CateringPage} />
-        <Route path="/contact" component={ContactPage} />
-
-        <Route component={ErrorPage} />
-      </Switch>
-    </Page.Wrapper>
-  );
-};
+}
 
 App.propTypes = {
   isLoading: PropTypes.bool,
 };
 
-export default App;
+// need react router dom
+export default withRouter(App);
